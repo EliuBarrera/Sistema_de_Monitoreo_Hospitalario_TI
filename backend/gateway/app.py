d1af34cc67f056300af2b0ead868338e206928f4
EliuBarrera <eliubarrejo@gmail.com>
2026-05-09 15:06:05 -0500

    Modified Gateway

--- backend/gateway/app.py
+++ backend/gateway/app.py
@@ -88,16 +88,5 @@ def location_detail(id):
         response = requests.delete(f"{SERVICE_URLS['locations']}/{id}")    
     return jsonify(response.json()), response.status_code
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    app.json.sort_keys = False
-    db.init_app(app)
-    app.register_blueprint(location_bp, url_prefix="/locations")
-    return app
-
 if __name__ == "__main__":
-    app = create_app()
-    with app.app_context():
-        db.create_all()
     app.run(port=5000, debug=True)
