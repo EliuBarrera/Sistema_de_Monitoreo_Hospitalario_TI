adc4341364c056c7650c911d345e26033e6b8a9e
EliuBarrera <eliubarrejo@gmail.com>
2026-05-08 20:01:56 -0500

    Model of location service and extensions file

---
+++ backend/locations_service/models/location_model.py
@@ -0,0 +1,12 @@
+from extensions import db
+
+class Location(db.Model):
+    __tablename__ = "locations"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    building = db.Column(db.String(50), nullable=False)
+    floor = db.Column(db.Integer, nullable=False)
+    room = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(50), nullable=False)
+    parent_location_id = db.Column(db.Integer, db.ForeignKey("location.id"))
+    
