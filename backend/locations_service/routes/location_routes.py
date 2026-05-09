05f503ae20814044f58dd276961efcc163eb4a11
EliuBarrera <eliubarrejo@gmail.com>
2026-05-09 03:40:20 -0500

    Location routes are created succesfully

---
+++ backend/locations_service/routes/location_routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify, request
+from controllers.location_controller import *
+
+location_bp = Blueprint('locations', __name__)
+
+@location_bp.route('/locations', methods=['GET'])
+def get_locations_route():
+    return jsonify(get_all_locations())
+
+@location_bp.route('/locations/<id>', methods=['GET'])
+def get_location_route(id):
+    return jsonify(get_location(id))
+
+@location_bp.route('/locations', methods=['POST'])
+def create_location_route():
+    return jsonify(create_location(request.json))
+
+@location_bp.route('/locations/<id>', methods=['PUT'])
+def update_location_route(id):
+    return jsonify(update_location(id, request.json))
+
+@location_bp.route('/locations/<id>', methods=['DELETE'])
+def delete_location_route(id):
+    return jsonify(delete_location(id))
+
