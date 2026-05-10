b553672e232bce9275f5a6a315e7ebc7bdcf5078
EliuBarrera <eliubarrejo@gmail.com>
2026-05-10 18:28:51 -0500

    Implement fully functional locations service with gateway integration
    * Model of location service and extensions file
    
    * Location controller is created succesfully
    
    * Location routes are created succesfully
    
    * Create App and gateway for locations_services
    
    * Modified Gateway
    
    * alert service completo (creo)
    
    * Bug fixes in auth_services
    
    * Fix auth and location service errors, add gateway endpoints, and validate functionality
    
    ---------
    
    Co-authored-by: TABORDA1234 <luicarlostabordaflorez@gmail.com>

---
+++ backend/locations_service/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from routes.location_routes import location_bp
+from config import Config
+from extensions import db
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.json.sort_keys = False
+db.init_app(app)
+app.register_blueprint(location_bp)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(port=5004, debug=True)
---
+++ backend/locations_service/models/location_model.py
@@ -0,0 +1,14 @@
+from extensions import db
+
+class Location(db.Model):
+    __tablename__ = "locations"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    building = db.Column(db.String(50), nullable=False)
+    floor = db.Column(db.Integer, nullable=True)
+    room = db.Column(db.String(50), nullable=True)
+    description = db.Column(db.String(50), nullable=False)
+    parent_location_id = db.Column(db.Integer, db.ForeignKey("locations.id"))
+
+    # Auto-referencia: remote_side=[id] le dice que 'id' es el lado "uno"
+    children = db.relationship('Location', backref=db.backref('parent', remote_side=[id]))
